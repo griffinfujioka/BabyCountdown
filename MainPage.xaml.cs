a1d875ba2a3a2832c905f8e785651e06dbdbc7dc
Griffin Fujioka <fujiokag@hotmail.com>
2013-01-03 19:26:22 -0700

    Little bit of progress, almost done
    The live tiles are working now and same with setting the delivery date.
    I need the same artwork for this app that I used in Graduation Countdown
    then just refine it towards babies before I publish it.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,7 +11,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using Windows.Storage;          // for ApplicationData
+using Windows.Globalization.DateTimeFormatting; // datetime formatting
+using Windows.ApplicationModel.Background;      // background tasks 
+using Windows.UI;
+using Clock.WinRT;
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
 namespace BabyCountdown
@@ -26,17 +30,50 @@ namespace BabyCountdown
         private Windows.Foundation.Collections.IPropertySet appSettings;
         private const string dateKey = "dateKey";
         private const string TASKNAMEUSERPRESENT = "TileSchedulerTaskUserPresent";
-        private const string TASKNAMETIMER = "TileSchedulerTaskTimerGraduation";
-        private const string TASKENTRYPOINT = "Clock.WinRT.TileSchedulerTaskGraduation";
+        private const string TASKNAMETIMER = "TileSchedulerTaskTimerDeliveryDate";
+        private const string TASKENTRYPOINT = "Clock.WinRT.TileSchedulerTaskDeliveryDate";
         public static DateTime DueDate;
         private DispatcherTimer timer;
         #endregion 
 
+        #region MainPage Constructor 
         public MainPage()
         {
             this.InitializeComponent();
-        }
+            appSettings = ApplicationData.Current.LocalSettings.Values;
 
+            Loaded += OnLoaded;
+
+            #region Initialize all selected values in the graduation date popup
+            monthComboBox.SelectedIndex = 0;
+            dayComboBox.SelectedIndex = 0;
+            yearComboBox.SelectedIndex = 0;
+            #endregion
+
+            #region Show the date PopUp if there isn't a saved date
+            if (!appSettings.ContainsKey(dateKey))
+            {
+                untilTxtBlock.Visibility = Visibility.Collapsed;
+                countdownTxtBlock.Visibility = Visibility.Collapsed;
+                datePopUp.IsOpen = true;
+            }
+            else
+            {
+                datePopUp.IsOpen = false;
+                string date = appSettings[dateKey].ToString();
+                var tempArray = date.Split(' ');    // Results in  tempArray[0] = xx/xx/xx
+                var dateArray = tempArray[0].Split('/');
+                int month = Convert.ToInt32(dateArray[0]);
+                int day = Convert.ToInt32(dateArray[1]);
+                int year = Convert.ToInt32(dateArray[2]);
+                DueDate = new DateTime(year, month, day);
+                //Clock.WinRT.ClockTileScheduler.SetGraduationDate(year, month, day); 
+            }
+            #endregion 
+        }
+        #endregion 
+
+        #region Load State 
         /// <summary>
         /// Populates the page with content passed during navigation.  Any saved state is also
         /// provided when recreating a page from a prior session.
@@ -49,7 +86,9 @@ namespace BabyCountdown
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
         }
+        #endregion 
 
+        #region Save State 
         /// <summary>
         /// Preserves state associated with this page in case the application is suspended or the
         /// page is discarded from the navigation cache.  Values must conform to the serialization
@@ -59,6 +98,79 @@ namespace BabyCountdown
         protected override void SaveState(Dictionary<String, Object> pageState)
         {
         }
+        #endregion 
+
+        #region OnNavigatedTo
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            grid.Background = new SolidColorBrush(Color.FromArgb(255, 0, 153, 255));
+        }
+        #endregion
+
+        #region OnLoaded
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            int CurrentYear = DateTime.Now.Year;                /* Get Current Year */
+            DateTime NewYear = new DateTime(DateTime.Now.Year + 1, 1, 1);
+
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            timer.Tick += new EventHandler<object>(OnTick);
+
+            try
+            {
+                timer.Start();
+                CreateClockTask();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        #endregion
+
+        #region OnTick
+        private void OnTick(object sender, object e)
+        {
+            var timeLeft = DueDate - DateTime.Now;
+
+            countdownTxtBlock.Text = string.Format("{0:D2} days\n{1:D2} hours\n{2:D2} minutes\n{3:D2} seconds", timeLeft.Days, timeLeft.Hours, timeLeft.Minutes, timeLeft.Seconds);
+        }
+        #endregion 
+
+        #region CreateClockTask
+        public static async void CreateClockTask()
+        {
+            try
+            {
+
+                var result = await BackgroundExecutionManager.RequestAccessAsync();
+                if (result == BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity ||
+                    result == BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity)
+                {
+                    foreach (var task in BackgroundTaskRegistration.AllTasks)
+                    {
+                        if (task.Value.Name == TASKNAMEUSERPRESENT)
+                            task.Value.Unregister(true);
+                    }
+                    ClockTileScheduler.CreateSchedule();
+
+                    BackgroundTaskBuilder builder = new BackgroundTaskBuilder();
+                    builder.Name = TASKNAMEUSERPRESENT;
+                    builder.TaskEntryPoint = TASKENTRYPOINT;
+                    builder.SetTrigger(new SystemTrigger(SystemTriggerType.UserPresent, false));
+                    builder.Register();
+                    var registration = builder.Register();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        #endregion 
 
         #region datePopUp Save Button Clicked
         private void saveDateBtn_Click(object sender, RoutedEventArgs e)
